2645f26eaaf9c14256c7e530e29a98e0fb0b9dc7
Carlos Rubio BM <crubio@bmdigital.co>
2018-09-03 11:58:23 -0500

    Formulario contacto

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MainService } from '../services/main.service';
 
 @Component({
     selector: 'app-contact',
@@ -8,7 +10,55 @@ import { Component, OnInit } from '@angular/core';
 export class ContactComponent implements OnInit {
     test: Date = new Date();
 
-    constructor() { }
+    rForm: FormGroup;
+    post: any;
+    nombre: string = '';
+    tel: string = '';
+    cel: string = '';
+    email: string = '';
+    ciudad: string = '';
+    mensaje: string = '';
+    titleAlert: string = 'Completa este campo';
+
+    constructor(private mainService: MainService, private fb: FormBuilder) {
+        this.rForm = fb.group({
+            'nombre': [null, Validators.compose([
+                Validators.required,
+                Validators.minLength(3),
+                Validators.maxLength(140)
+            ])],
+            'cel': [null, Validators.compose([
+                Validators.required,
+                Validators.minLength(7),
+                Validators.maxLength(7)
+            ])],
+            'tel': [null, Validators.compose([
+                Validators.required,
+                Validators.minLength(10),
+                Validators.maxLength(10)
+            ])],
+            'email': [null, Validators.compose([
+                Validators.required,
+                Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+            ])],
+            'ciudad': [null, Validators.compose([
+                Validators.required
+            ])],
+            'ocupacion': [null, Validators.compose([
+                Validators.required
+            ])],
+            'mensaje': [null, Validators.compose([
+                Validators.required,
+                Validators.minLength(20),
+                Validators.maxLength(340),
+            ])]
+        });
+    }
+
+    public sendData(lead) {
+        console.log(lead);
+        this.mainService.sendLead(lead);
+    }
 
     ngOnInit() {}
 }
